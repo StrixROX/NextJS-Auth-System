fa1d92e2fc542ffc2308069fe372f9e74200387a
StrixROX <40167899+StrixROX@users.noreply.github.com>
2024-01-01 00:06:12 +0530

    Added signin page

---
+++ src/app/users/signin/page.js
@@ -0,0 +1,38 @@
+'use client'
+
+import styles from '../form.module.css'
+
+import { redirect } from "next/navigation"
+import SignInHandler from "./signin"
+import { useState } from 'react'
+
+export default function SignUpPage() {
+  const [errors, setErrors] = useState([])
+
+  async function handleSubmit(formData) {
+    const { success, msgs } = await SignInHandler(formData)
+    if (!success) {
+      setErrors(msgs)
+      return false
+    }
+
+    redirect('/users', 'push')
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.card}>
+        <img src="/images/banner.webp" alt="Banner" className={styles.banner} />
+        <div className={styles.formWrapper}>
+          <form action={handleSubmit} className={styles.form}>
+            <h1 className={styles.title}>Sign in</h1>
+            <span className={styles.error}>{errors.join('\n')}</span>
+            <input type="text" name="username" placeholder="Username" required tabIndex={1} />
+            <input type="password" name="password" placeholder="Password" required tabIndex={2} />
+            <input type="submit" value="Sign in" />
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/users/signin/signin.js
@@ -0,0 +1,38 @@
+'use server'
+
+import { prisma } from '@/app/prisma'
+import bcrypt from 'bcrypt'
+
+export default async function SignInHandler(formData) {
+  const username = formData.get('username')
+  const password = formData.get('password')
+
+  let user = null
+  let isCorrectPassword = false
+  let msgs = []
+  try {
+    user = await prisma.users.findUnique({
+      where: {
+        username
+      }
+    })
+  } catch (err) {
+    msgs.push('An error occured')
+  }
+
+  if (user === null) {
+    msgs.push('Invalid username')
+  }
+  else {
+    isCorrectPassword = await bcrypt.compare(password, user.password)
+
+    if (!isCorrectPassword) {
+      msgs.push('Incorrect password')
+    }
+  }
+
+  return {
+    success: isCorrectPassword,
+    msgs
+  }
+}
