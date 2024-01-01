8cd5cf435417ada3fb559bcfd9aa1c72191aa3fe
StrixROX <40167899+StrixROX@users.noreply.github.com>
2024-01-01 17:17:02 +0530

    Removed users GET route

--- src/app/users/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { prisma } from "../prisma";
-
-export async function GET() {
-  const users = await prisma.users.findMany()
-  return NextResponse.json(users)
-}
